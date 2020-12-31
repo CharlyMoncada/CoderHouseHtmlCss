aae3c00d843b9625bceeb7e94afcf5b2481ff257
Carlos Moncada <moncadacarlos89@gmail.com>
2020-12-30 21:28:00 -0300

    Added ajax for footer

---
+++ Code/javascript/footer.js
@@ -0,0 +1,33 @@
+let html = "";
+let contenido = [];
+
+function cargoRedesSocialesStreaming() {
+  $.ajax({
+    url: "javascript/json/footer.json",
+    dataType: "json",
+    success: function (response) {
+        contenido = response;
+      $.each(contenido, function (i) {
+          debugger
+        html += `<li class="footer__list footer__item--${contenido[i].redSocial}">
+                        <a
+                            id="${contenido[i].id}"
+                            class="footer__list__link"
+                            href="${contenido[i].href}"
+                            target="_blank"
+                        >
+                        <img src="${contenido[i].rutaImagen}" />
+                        </a>
+                 </li>`
+      });
+      $("#footer__list").html(html);
+    },
+    error: function () {
+      console.error("Ocurrió un error... :(");
+      html = `<option value=0>" Error cargando Json "</option>`;
+      $("#footer__list").html(html);
+    },
+  });
+}
+
+setTimeout(() => cargoRedesSocialesStreaming());
